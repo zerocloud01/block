8d9444520e8249bdc797bda94cffb61eadfccc02
zerocloud01 <zerocloud01@outlook.com>
2025-04-09 14:08:32 +0800

    修改[F

--- bat/easy/bat.cpp
+++ bat/easy/bat.cpp
@@ -7,10 +7,9 @@ int main()
     int T = 1e3; // 数据次数，也可以写死循环直到wa。
     for(int i=1;i<=100;++i)
     {
-        cout << "test:" << i << '\n';
         system("generater.exe > data.in");
         system("std.exe < data.in > std.out");
         system("solve.exe < data.in > solve.out");
-        if(system("fc std.out solve.out > diff.log"))   cout << "WA\n";
+        if(system("fc std.out solve.out > diff.log"))   cout << "test" << i << "WA\n";
     }
 }
